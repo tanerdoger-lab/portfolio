917df9511ca4edcf3d0f89d985aeddae18a33708
tanerdoger-lab <tanerdoger@gmail.com>
2026-03-15 19:18:33 +0300

    Update Portfolio.jsx

--- Portfolio.jsx
+++ Portfolio.jsx
@@ -46,6 +46,7 @@ const photographyWorks = [
   { title: "Ruhsar", description: "Black and White on Paper — 53 x 40.6 cm. Published on 1x.", image: "/images/7.jpg" },
   { title: "Ferries of Istanbul", description: "Black and White on Paper — 46 x 29.3 cm. Published on 1x.", image: "/images/8.jpg" },
   { title: "The Domes", description: "Black and White on Paper — 33.3 x 45.8 cm. Published on 1x.", image: "/images/9.jpg" },
+  { title: "The Cat", description: "Black and White on Paper — 35.6 x 41.4 cm. Published on 1x.", image: "/images/17.jpg" },
   { title: "The Urban Passage", description: "Black and White on Paper — 31.1 x 51.1 cm. Accepted on 1x.", image: "/images/10.jpeg" },
   { title: "Street Musicians", description: "Black and White on Paper — 34.4 x 55.3 cm. Accepted on 1x.", image: "/images/11.jpeg" },
   { title: "The Door", description: "Black and White on Paper — 40.6 x 40.6 cm. Accepted on 1x.", image: "/images/12.jpg" },
